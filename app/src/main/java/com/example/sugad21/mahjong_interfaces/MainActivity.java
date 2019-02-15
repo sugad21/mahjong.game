12549ceeabb5c746c7941173575cfd9de1820777
sugad21 <sugad21@up.edu>
2019-02-14 20:26:54 -0800

    Initial commit

---
+++ app/src/main/java/com/example/sugad21/mahjong_interfaces/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sugad21.mahjong_interfaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
